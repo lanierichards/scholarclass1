3f3fbbb003b2d4bf942548038cb6e41cd8eed586
Allison He <allisonhe16@gmail.com>
2019-07-09 14:14:10 -0700

    Second Commit

--- Contents.swift
+++ Contents.swift
@@ -5,17 +5,28 @@ class Scholar {
     
     var grade = 11
     var studying = "Swift"
-    var name = "Halle"
+    var name = "default"
+    var favoriteClass = "default"
     
+    //The init function will run with EVERY class, meaning that we put properties there that every single objet needs to have.
     
-    init(scholarName : String) {
+    init(scholarName : String, scholarGrade : Int, scholarStudying : String, scholarFavoriteClass : String) {
         name = scholarName
-        
+        grade = scholarGrade
+        studying = scholarStudying
+        favoriteClass = scholarFavoriteClass
     }
 }
-var newScholar = Scholar(scholarName: "Kyla")
 
-print(newScholar.grade)
+//you create your objects OUTSIDE of your class! You specialize each blueprint
+var kylaScholar = Scholar(scholarName: "Kyla", scholarGrade : 10, scholarStudying : "Swift", scholarFavoriteClass: "Math")
+var hunterScholar = Scholar(scholarName: "Hunter", scholarGrade: 12, scholarStudying: "Swift", scholarFavoriteClass: "English")
 
-
-
+print(kylaScholar.name)
+print(kylaScholar.grade)
+print(kylaScholar.studying)
+print(kylaScholar.favoriteClass)
+print(hunterScholar.name)
+print(hunterScholar.grade)
+print(hunterScholar.studying)
+print(hunterScholar.favoriteClass)
